a4593b18c617f1cdac773b3f57e6860ae4d55d2e
Evan <49596225+ezm42069@users.noreply.github.com>
2024-05-02 11:58:04 -0500

    first commit again

---
+++ content/my-first-post.md
@@ -0,0 +1,10 @@
+Title: My First Post
+Date: 2024-04-18
+Category: Python
+Tags: pelican, python
+Slug: my-first-post
+Authors: Evan Ma
+
+This is my first post using Pelican!
+
+![Python logo](/images/python-logo.png)
---
+++ pelicanconf.py
@@ -0,0 +1,41 @@
+AUTHOR = 'Christy Goon, Evan Ma, and Konrad Ochwat'
+SITENAME = 'Disney and Pixar Animated Film Casting Diversity'
+SITEURL = 'https://Evimess.github.io/disney-diversity'
+
+PATH = "content"
+
+TIMEZONE = 'America/Chicago'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+
+# Blogroll
+LINKS = (
+    ("A Course Project", "#"),
+    ("IS310 Course Website", "https://zoeleblanc.com/is310-computing-humanities-2024/"),
+)
+
+# Social widget
+SOCIAL = (
+    ("You can add links in your config file", "#"),
+    ("Another social link", "#"),
+)
+
+DEFAULT_PAGINATION = 10
+
+STATIC_PATHS = ['images']
+
+THEME = 'themes/alchemy'
+THEME_CSS_OVERRIDES = ['themes/css/oldstyle.css']
+
+
+# Uncomment following line if you want document-relative URLs when developing
+#coment again when pushing ot github
+#RELATIVE_URLS = True
